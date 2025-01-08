8223648b8700038096a39482d2c3f490ed010f51
Renderson99 <rendersonbatista19@gmail.com>
2025-01-08 15:53:12 -0300

    js

--- exercicio-projeto/ex014/script.js
+++ exercicio-projeto/ex014/script.js
@@ -1,1 +1,11 @@
-alert("Ola")
+function verificar() {
+    var data = new Data()
+    var ano = data.getFullyear()
+    var fano = document.getElementById("txtano")
+    var res = document.querySelector("div#res")
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente!")
+    } else {
+        window.alert("tudo ok")
+    }
+}
