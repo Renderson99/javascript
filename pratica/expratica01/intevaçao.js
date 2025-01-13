4d206fd77866e775949507984ed4020b575f946b
Renderson99 <rendersonbatista19@gmail.com>
2025-01-13 19:18:02 -0300

    pratica de javascript concluida

--- pratica/expratica01/intevaçao.js
+++ pratica/expratica01/intevaçao.js
@@ -1,29 +1,27 @@
 function contar() {
     let ini = document.getElementById("txti")
     let fim = document.getElementById("txtf")
-    let passo = document.getElementById('txtp')
+    let passo = document.getElementById("txtp")
     let res = document.getElementById("res")
-
+    
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert("Passo inválido! Considerando o PASSO 1")
-        res.innerHTML = `Inposscivel contar!`
+        window.alert("Dados invállidos!")
+        res.innerHTML = `Dados inválidos!`
     } else {
-        res.innerHTML = "Contando: <br> "
+        res.innerHTML = `Contando: <br>`
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        
         if (i < f) {
-            // Contagem crescente
-         for(let c = i;c <= f; c+= p) {
-            res.innerHTML += ` ${c} \u{1f449}`
-            } 
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
         } else {
-            //Contagem regressiva
-            for(let c = i;c >= f;c -= p) {
+            for(let c = i; c >= f; c -= p) {
                 res.innerHTML += `${c} \u{1f449}`
             }
         }
-        res.innerHTML +=`\u{1f3c1}`
-        
+        res.innerHTML += `\u{1f3c1}`
     }
 }
