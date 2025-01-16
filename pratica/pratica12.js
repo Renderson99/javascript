5af1214450916f40b2cfa32f24b5da559c3bddb9
Renderson99 <rendersonbatista19@gmail.com>
2025-01-16 14:27:12 -0300

    funções em javascript

---
+++ pratica/pratica12.js
@@ -0,0 +1,14 @@
+function soma (n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma(10, 5))
+
+function divisão ( n1=0, n2=0) {
+    return n1 % n2
+}
+console.log(divisão(100, 2))
+
+function multplicação (n1=0, n2=0) {
+    return n1 * n2
+}
+console.log(multplicação(10, 2))
