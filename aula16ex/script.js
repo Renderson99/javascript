7e566ab707723f353d74e7a18d9ceb77a1a70e1c
Renderson99 <rendersonbatista19@gmail.com>
2025-01-22 19:03:07 -0300

    aula 16ex de javascript

---
+++ aula16ex/script.js
@@ -0,0 +1,27 @@
+let num = document.querySelector("input#fnum")
+let lista = document.querySelector("select#flista")
+let res = document.querySelector("div#res")
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <- 100) {
+            return true
+        } else {
+            return false
+        }
+}
+function isLista (n, l) {
+    if (l.indexof(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !lista(num.value, valores)) {
+        window.alert("deu certo")
+    }else {
+       window.alert("Valor inválido ou já encontrado na lista.") 
+    }
+}
