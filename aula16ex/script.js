5ecc481a57dc594ac74ed055201659b8caaf5ef2
Renderson99 <rendersonbatista19@gmail.com>
2025-01-25 17:28:06 -0300

    aula 16ex finalizada

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -24,7 +24,35 @@ function adicionar() {
        let item = document.createElement("option")
        item.texte = `Valor ${num.valores} adicionado.`
        lista.appendChild(item)
+       res.innerHTML = ""
     }else {
        window.alert("Valor inválido ou já encontrado na lista.") 
+    }
+    num.value = ""
+    num.focus()
+}
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert("Adicione valores antes de finalizar!")
+    } else {
+        let tot = valores.length
+        let maior = [0]
+        let menor = [0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ""
+        res.innerHTML += `<p> Ao todo, temos ${tot} número cadastrados.</p> `
+        res.innerHTML += `<p>Omaior valor digitado foi ${maior}</p>`
+        res.innerHTML += `O menor valor digitado foi ${menor}`
+        res.innerHTML += `Somando todos os valores, temos ${soma}`
+        res.innerHTML += `A média dos valores digitados é ${media}`
     }
 }
