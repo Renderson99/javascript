1009e93d2a4af43e702f5d1e439484384696310a
Renderson99 <rendersonbatista19@gmail.com>
2025-02-15 18:16:38 -0300

    aula 32 de javascript

---
+++ cfb_cursos/aula32/aula32.js
@@ -0,0 +1,13 @@
+const cursoTodos = [...document.getElementsByClassName("curso")]
+const cursoC1 = [...document.getElementsByClassName("c1")]
+const cursoC2 = [...document.getElementsByClassName("C2")]
+const cursoEspecial = document.getElementsByClassName("curso")[6]
+
+console.log(cursoTodos)
+console.log(cursoC1)
+console.log(cursoC2)
+console.log(cursoEspecial)
+
+cursoC2.map((el => {
+    el.classList.add("destaque")
+}))
