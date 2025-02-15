83fa88ae8b05aefafd9bb44f8f787f2619c4e8e7
Renderson99 <rendersonbatista19@gmail.com>
2025-02-15 18:50:38 -0300

    aula 33 de js feita

---
+++ cfb_cursos/aula33/aula33.js
@@ -0,0 +1,15 @@
+const divTodos = [...document.getElementsByClassName("div")]
+const cursoTodos = [...document.getElementsByClassName("curso")]
+const cursoC1 = [...document.getElementsByClassName("c1")]
+const cursoC2 = [...document.getElementsByClassName("C2")]
+const cursoEspecial = document.getElementsByClassName("curso")[6]
+
+console.log(divTodos)
+console.log(cursoTodos)
+console.log(cursoC1)
+console.log(cursoC2)
+console.log(cursoEspecial)
+
+//cursoC2.map((el => {
+    //el.classList.add("destaque")
+//}))
