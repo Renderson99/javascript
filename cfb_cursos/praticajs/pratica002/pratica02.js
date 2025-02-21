5c64cbbb8784932d66e8200056fca4aa69fa4e4a
Renderson99 <rendersonbatista19@gmail.com>
2025-02-21 16:58:22 -0300

    pratica de flexbox

---
+++ cfb_cursos/praticajs/pratica002/pratica02.js
@@ -0,0 +1,14 @@
+const caixa = document.querySelector("#caixa")
+const bt_c1 = document.querySelector("#c1")
+ console.log(caixa.firstElementChild)
+ console.log(caixa.lastElementChild)
+ console.log(caixa.getRootNode())
+ console.log(bt_c1.firstChild)
+ console.log(bt_c1.lastElementChild)
+
+/* APRENDENDO:
+    .firstElementChild
+    .lastElementChild
+    .firstChild
+    .lastElementChild
+*/
