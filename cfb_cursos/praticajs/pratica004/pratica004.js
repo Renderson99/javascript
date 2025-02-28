0898a1d1f919c75703236194db663bef335f1338
Renderson99 <rendersonbatista19@gmail.com>
2025-02-28 14:42:04 -0300

    pratica 004  de js

---
+++ cfb_cursos/praticajs/pratica004/pratica004.js
@@ -0,0 +1,7 @@
+let div1 = document.getElementById("div#1")
+let div2 = document.getElementById("div#2")
+let p = document.querySelector("main")
+
+function click1() {
+    div1.style.display = "none"
+}
