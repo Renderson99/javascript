f7de2bcc7f94be989e0eeac2d79153148cdd3e59
Renderson99 <rendersonbatista19@gmail.com>
2025-03-11 17:08:57 -0300

    pratica de js

---
+++ pratica/pratica22.js
@@ -0,0 +1,6 @@
+let nome = "Renderson"
+let sobrenome = "Batista dos Santos"
+let msg = () => {
+    console.log (`O seu primeiro nome é ${nome} é o sobrenome ${sobrenome} então o nome completo é ${nome} ${sobrenome}`)
+}
+msg()
