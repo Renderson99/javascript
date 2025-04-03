ba1e974117ba321171c57bb896843c0dced298e0
Renderson99 <rendersonbatista19@gmail.com>
2025-04-03 15:47:27 -0300

    praticando promise

--- cfb_cursos/praticajs/pratica005/promese.js
+++ cfb_cursos/praticajs/pratica005/promese.js
@@ -1,7 +1,14 @@
 const numero = document.getElementById("numero")
+const btn_promessa = document.getElementById("btn_promessa")
 
-let promesa = new Promise((resolvido,rejeitado) => {
-    let resultado = true
+btn_promessa.addEventListener("click",(evet) =>{
+    numero.innerHTML = "Processando..."
+    promessa()
+})
+
+const promessa = ()=>{
+   let promesa = new Promise((resolvido,rejeitado) => {
+    let resultado = false
     let tempo = 1000
     setTimeout(()=>{
         if (resultado) {
@@ -10,18 +17,20 @@ let promesa = new Promise((resolvido,rejeitado) => {
             rejeitado("Deu errado")
         }
     },tempo)
-})
+    })
 
-promesa.then((retorno)=>{
-    numero.innerHTML = retorno
-    numero.classList.remove("erro")
-    numero.classList.add("ok")
-})
+    promesa.then((retorno)=>{
+        numero.innerHTML = retorno
+        numero.classList.remove("erro")
+        numero.classList.add("ok")
+    })
 
-promesa.catch((retorno)=>{
-    numero.innerHTML = retorno
-    numero.classList.add("erro")
-    numero.classList.remove("ok")
-})
+    promesa.catch((retorno)=>{
+        numero.innerHTML = retorno
+        numero.classList.add("erro")
+        numero.classList.remove("ok")
+    }) 
+}
 
-numero.innerHTML = "Processando..."
+numero.innerHTML = "Esperando..."
+
