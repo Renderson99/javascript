0c6469f6420e7e8f827b2d4525580484f436b743
Renderson99 <rendersonbatista19@gmail.com>
2025-04-21 12:14:04 -0300

    pratica43 e 44

---
+++ pratica/pratica44.js
@@ -0,0 +1,23 @@
+const nave = function(energia) {
+    this.energia = energia
+    this.disparos = 20
+
+}
+
+const n1 = new nave(100)
+
+nave.prototype.vidas = 3
+nave.prototype.disparar = function() {
+    if(this.disparos > 0) {
+        this.disparos--
+    }
+}
+
+n1.disparar()
+n1.disparar()
+n1.disparar()
+n1.disparar()
+n1.disparar()
+console.log(nave)
+console.log(n1)
+console.log(n1.disparos)
